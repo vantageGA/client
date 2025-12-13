bb081ecb0faf2e2a67a62319560d62230c037682
StoryDevGA <gary@storylineos.com>
2025-12-13 18:44:52 +0000

    Use env-based API base URL

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
 import store from './store/store';
+
+// Use a configurable API base URL so production can hit Render and dev can proxy locally
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+if (apiBaseUrl) {
+  axios.defaults.baseURL = apiBaseUrl;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
